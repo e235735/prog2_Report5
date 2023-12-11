a755e0129c53a06b1fa51bb3187d516dfd167a76
Tsubasa Kaneku <e235735@ie.u-ryukyu.ac.jp>
2023-12-12 01:11:57 +0900

    2nd commit

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,11 @@
 public class Main {
     public static void main(String[] args) {
-        String str = null;
-
-        System.out.println(str.length());
+        try {
+            String str = null;
+            System.out.println(str.length());
+        } catch (NullPointerException e) {
+            System.out.println("NullPointerExceptionエラーが発生しました。");
+            System.out.println("エラーメッセージ：" + e.getMessage());
+        }
     }
 }
